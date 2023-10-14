f5ffd0d8af59a24dbee1871230e142a16b8e62e2
Zach K <huntingmanzach@gmail.com>
2023-10-13 23:01:58 -0400

    condense more lists

--- scripts/yamlfix.py
+++ scripts/yamlfix.py
@@ -4,13 +4,13 @@ import yaml
 import hjson
 import re
 
-LIST = re.compile(r"\n((\s*)[^:\s]+:)\n((?:\2 +- .*?\n)+)")
+LIST = re.compile(r"(?<=\n)((\s*)[^:\s]+:)\n((?:\2 +- .*?\n)+)")
 
 def replm(m: re.Match):
     if m[3].count("- ") >= 7: return m[0]
     if m[3].count(": "): return m[0]
     
-    s = "\n" + m[1] + " [" + ", ".join([i.strip() for i in m[3].split("- ") if i.strip()]) + "]\n"
+    s = m[1] + " [" + ", ".join([i.strip() for i in m[3].split("- ") if i.strip()]) + "]\n"
 
     return s
 
