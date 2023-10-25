affe57b9b830de64fbd650bf3b7fda3d344cf089
Zach K <huntingmanzach@gmail.com>
2023-10-25 04:35:30 -0400

    Fix more 1.20.2 errors

--- scripts/yamltest.py
+++ scripts/yamltest.py
@@ -1,6 +1,11 @@
 import yaml
 import glob
 import qfile
+import traceback
 
 for f in glob.glob("*/*.yaml"):
-    yaml.safe_load(qfile.read(f))
+    try:
+        yaml.safe_load(qfile.read(f))
+    except Exception as e:
+        print(f'Error loading file "{f}":')
+        print("  " + str(e))
